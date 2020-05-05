0ddc96a9b0d47be45e2b4e005e7cf4abf0dcde7f
EC2 Default User <ec2-user@ip-172-31-30-93.us-east-2.compute.internal>
2020-05-05 07:45:23 +0000

    japanese

--- config/environments/application.rb
+++ config/environments/application.rb
@@ -2,9 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 module SampleApp
   class Application < Rails::Application
-    # 認証トークンをremoteフォームに埋め込む
+        # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
-    config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
