13cc7c0a7257d721687ad069d34a0f51bf2ee857
EC2 Default User <ec2-user@ip-172-31-30-93.us-east-2.compute.internal>
2020-05-09 14:25:41 +0000

    Add css

--- app/assets/javascripts/bubbly.js
+++ app/assets/javascripts/bubbly.js
@@ -1,64 +1,46 @@
-window.bubbly = function (config) {
-    const c = config || {};
-    const r = () => Math.random();
-    const canvas = c.canvas || document.createElement("canvas");
-    let width = canvas.width;
-    let height = canvas.height;
-    if (canvas.parentNode === null) {
-        canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;min-width:100vw;min-height:100vh;");
-        width = canvas.width = window.innerWidth;
-        height = canvas.height = window.innerHeight;
-        document.body.appendChild(canvas);
-    }
-    const context = canvas.getContext("2d");
-    context.shadowColor = c.shadowColor || "#fff";
-    context.shadowBlur = c.blur || 4;
-    const gradient = context.createLinearGradient(0, 0, width, height);
-    gradient.addColorStop(0, c.colorStart || "#2AE");
-    gradient.addColorStop(1, c.colorStop || "#17B");
-    const nrBubbles = c.bubbles || Math.floor((width + height) * 0.02);
-    const bubbles = [];
-    for (let i = 0; i < nrBubbles; i++) {
-        bubbles.push({
-            f: (c.bubbleFunc || (() => `hsla(0, 0%, 100%, ${r() * 0.1})`)).call(), // fillStyle
-            x: r() * width, // x-position
-            y: r() * height, // y-position
-            r: (c.radiusFunc || (() => 4 + r() * width / 25)).call(), // radius
-            a: (c.angleFunc || (() => r() * Math.PI * 2)).call(), // angle
-            v: (c.velocityFunc || (() => 0.1 + r() * 0.5)).call() // velocity
-        });
-    }
-    (function draw() {
-        if (canvas.parentNode === null) {
-            return cancelAnimationFrame(draw)
-        }
-        if (c.animate !== false) {
-            requestAnimationFrame(draw);
-        }
-        context.globalCompositeOperation = "source-over";
-        context.fillStyle = gradient;
-        context.fillRect(0, 0, width, height);
-        context.globalCompositeOperation = c.compose || "lighter";
-        bubbles.forEach(bubble => {
-            context.beginPath();
-            context.arc(bubble.x, bubble.y, bubble.r, 0, Math.PI * 2);
-            context.fillStyle = bubble.f;
-            context.fill();
-            // update positions for next draw
-            bubble.x += Math.cos(bubble.a) * bubble.v;
-            bubble.y += Math.sin(bubble.a) * bubble.v;
-            if (bubble.x - bubble.r > width) {
-                bubble.x = -bubble.r;
-            }
-            if (bubble.x + bubble.r < 0) {
-                bubble.x = width + bubble.r;
-            }
-            if (bubble.y - bubble.r > height) {
-                bubble.y = -bubble.r;
-            }
-            if (bubble.y + bubble.r < 0) {
-                bubble.y = height + bubble.r;
-            }
-        });
-    })();
+// js only creates and insert parts + click events adds classes
+
+var numOfPieces = 6 * 6;
+
+var frag = document.createDocumentFragment();
+
+function insertInnerPieces($el, innerPieces) {
+  for (var i = 0; i < innerPieces; i++) {
+    var $inner = document.createElement('div');
+    $inner.classList.add('popup__piece-inner');
+    $el.appendChild($inner);
+  }
 };
+
+for (var i = 1; i <= numOfPieces; i++) {
+  var $piece = document.createElement('div');
+  $piece.classList.add('popup__piece');
+  
+  insertInnerPieces($piece, 3);
+  frag.appendChild($piece);
+}
+
+document.querySelector('.popup__pieces').appendChild(frag);
+
+var $popupsCont = document.querySelector('.popups-cont');
+var $popup = document.querySelector('.popup');
+var popupAT = 900;
+
+document.querySelector('.popup-btn').addEventListener('click', function() {
+  $popupsCont.classList.add('s--popup-active');
+  $popup.classList.add('s--active');
+});
+
+function closeHandler() {
+  $popupsCont.classList.remove('s--popup-active');
+  $popup.classList.remove('s--active');
+  $popup.classList.add('s--closed');
+  
+  setTimeout(function() {
+    $popup.classList.remove('s--closed');
+  }, popupAT);
+}
+
+document.querySelector('.popup__close').addEventListener('click', closeHandler);
+
+document.querySelector('.popups-cont__overlay').addEventListener('click', closeHandler);
