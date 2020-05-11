5ec60933c805a486b84452401aca036dffd84d01
EC2 Default User <ec2-user@ip-172-31-30-93.us-east-2.compute.internal>
2020-05-11 07:37:33 +0000

    Add sraidshow

--- app/assets/javascripts/bubbly.js
+++ app/assets/javascripts/bubbly.js
@@ -10,7 +10,7 @@ function insertInnerPieces($el, innerPieces) {
     $inner.classList.add('popup__piece-inner');
     $el.appendChild($inner);
   }
-};
+}
 
 for (var i = 1; i <= numOfPieces; i++) {
   var $piece = document.createElement('div');
@@ -26,10 +26,10 @@ var $popupsCont = document.querySelector('.popups-cont');
 var $popup = document.querySelector('.popup');
 var popupAT = 900;
 
-document.querySelector('.popup-btn').addEventListener('click', function() {
+document.addEventListener("turbolinks:load", function() {document.querySelector('.popup-btn').addEventListener('click', function() {
   $popupsCont.classList.add('s--popup-active');
   $popup.classList.add('s--active');
-});
+});});
 
 function closeHandler() {
   $popupsCont.classList.remove('s--popup-active');
