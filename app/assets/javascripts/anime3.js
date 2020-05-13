bfcb80c659c6d819dbb6a5f43267e4cd2e8a17fc
EC2 Default User <ec2-user@ip-172-31-30-93.us-east-2.compute.internal>
2020-05-13 14:22:18 +0000

    Add animetion

---
+++ app/assets/javascripts/anime3.js
@@ -0,0 +1,12 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  });
