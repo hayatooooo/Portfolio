c5568b8f4543fbd21b9990f2ace7dd34a76b970a
EC2 Default User <ec2-user@ip-172-31-30-93.us-east-2.compute.internal>
2020-05-13 13:42:36 +0000

    Add animetion

--- app/assets/javascripts/anime.js
+++ app/assets/javascripts/anime.js
@@ -5,11 +5,11 @@ var a = anime.timeline({loop:2})
     opacity: [0,1],
     easing: "easeOutCirc",
     duration: 1000,
-    delay: (el, i) => 1000 * i
+    delay: (el, i) => 1300 * i
   }).add({
     targets: '.ml15',
     opacity: 0,
-    duration: 1000,
+    duration: 1300,
     easing: "easeOutExpo",
-    delay: 5000
+    delay: 4000
   });
